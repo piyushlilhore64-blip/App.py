bedfd64f3fe8fc6e9751f0c2719236211e9adaef
piyushlilhore64-blip <piyushlilhore64@gmail.com>
2025-09-13 22:17:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,25 +1,8 @@
-import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-def get_connection():
-    conn = sqlite3.connect("users.db", detect_types=sqlite3.PARSE_DECLTYPES)
-    conn.row_factory = sqlite3.Row
-    return conn
+DATABASE_URL = "sqlite:///./users.db"
 
-def init_db():
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT UNIQUE NOT NULL,
-            password_hash TEXT NOT NULL,
-            time_limit INTEGER NOT NULL,
-            login_start TIMESTAMP,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-    """)
-    conn.commit()
-    conn.close()
-
-# Initialize DB
-init_db()
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
